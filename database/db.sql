4d7e910abdd39f517a51288ccfa4e1b4bf94f3d8
lauraortizolmos <laura.ortizolmos@gmail.com>
2020-04-29 16:11:31 -0500

    Autenticacion NOdejs

---
+++ database/db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users ( 
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(16) NOT NULL, 
+    email VARCHAR(100) NOT NULL, 
+    password BINARY(60) NOT NULL )
+
+    ALTER TABLE users
+ADD CONSTRAINT UC_User UNIQUE (email,username);
+    
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` mediumtext COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB
---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'ZwDFngFlxA',
+        password: '3Wctp6Peiw',
+        database: 'ZwDFngFlxA'
+    }
+
+};
